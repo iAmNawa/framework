5db6622fed7e3fbd94934c1b437a77e53953ec8f
Paul Borawski <paulborawski14@gmail.com>
2018-02-11 13:46:35 -0800

    returning this makes it chainable

--- Greetr.js
+++ Greetr.js
@@ -16,7 +16,46 @@
     es: 'Saludos'
   };
 
-  Greetr.prototype = {};
+   var logMessages = {
+     en: 'Logged in',
+     es: 'Inició sesión'
+   }
+
+  Greetr.prototype = {
+
+   fullName: function() {
+     return this.firstName + ' ' + this.lastName;
+   },
+
+   validate: function() {
+     if (supportedLangs.indexOf(this.language) === -1){
+       throw "Invalid language";
+     }
+   },
+
+   greeting: function() {
+     return greetings[this.language] + this.firstName + '!';
+   },
+
+   formalGreeting: function() {
+     return formalGreetings[this.language] + this.fullName();
+   },
+
+   greet: function(formal) {
+     var msg;
+     if (formal) {
+       msg = this.formalGreeting();
+     }
+     else {
+       msg = this.greeting();
+     }
+     if (console) {
+       console.log(msg);
+     }
+     return this;
+   }
+
+  };
 
   Greetr.init = function(firstName, lastName, language) {
 
